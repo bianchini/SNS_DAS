2809384883529f5f3aca2329f6e5602c826788f3
Lorenzo Bianchini <lore.bianchini@gmail.com>
2019-11-14 17:36:49 +0100

    variable NDIM

--- minimizer.C
+++ minimizer.C
@@ -2,54 +2,89 @@
 #include "Math/Factory.h"
 #include "Math/Functor.h"
 #include "TString.h"
+#include <iostream>
+#include "TRandom3.h"
 
-//TMatrixD B = THilbertMatrixD(4,4);
-const double elements[16] = 
+
+/*
+  const double elements[16] = 
   {1.0, 0.1, -0.1, 0.2, 
-   0.1, 1., 0., 0.0,
-   -0.1, 0., 1., 0.4,
-   0.2, 0., 0.4, 1.
+  0.1, 1., 0., 0.0,
+  -0.1, 0., 1., 0.4,
+  0.2, 0., 0.4, 1.
   };
-TMatrixDSym B(4, elements);
+  TMatrixDSym B(4, elements);
+  const double grad[4] = {-0.3, -2., -0.5, 2.0};
+*/
 
-const double grad[4] = {-0.3, -2., -0.5, 2.0};
-TVectorD g(4, grad);
+const int NDIM = 5;
+
+TMatrixD B = THilbertMatrixD(NDIM,NDIM);
+
+double grad[NDIM] = {};
+TVectorD g(NDIM, grad);  
 const double f0 = 1.0;
 
-double Quadratic(const double *xx )
+double Quadratic(const double *xx)
 {
-  TVectorD x(4, xx);
+  TVectorD x(NDIM, xx);
   return f0 + g*x + 0.5*(x*(B*x));
 }
 
 void minimizer(const char * minName = "Minuit", const char *algoName = "Migrad"){
+
+  TRandom3 ran(1234);
+  double gmin = -1.;
+  double gmax = +1.;
+  for(unsigned int i=0 ; i<NDIM; ++i){
+    double r = ran.Rndm();
+    g[i] = gmin*r + gmax*(1-r);
+  }
+
+  TVectorD eig(NDIM);
+  TMatrixD eigs = B.EigenVectors(eig);
+  Double_t det = B.Determinant();
+  std::cout << "Determinant of B: " << det << std::endl;
+  eig.Print();
+  eigs.Print();
+  if(det==0.){
+    std::cout << "Matrix is singular" << std::endl;
+  }
+  else if(eig.Min()>0.){
+    std::cout << "Matrix is positive definite" << std::endl;
+  }
+  else if(eig.Min()<0.){
+    std::cout << "Matrix is undefinite" << std::endl;
+  }
 
   ROOT::Math::Minimizer* minimum = ROOT::Math::Factory::CreateMinimizer(minName, algoName);
   minimum->SetMaxFunctionCalls(1000000); 
   minimum->SetTolerance(0.001);
   minimum->SetPrintLevel(1);
 
-  ROOT::Math::Functor f( &Quadratic, 4);
-  double step[4] = {0.01, 0.01, 0.01, 0.01};
-  double start[4] = { 0., 0., 0., 0.};
+  ROOT::Math::Functor f( &Quadratic, NDIM);
+  double step[NDIM] = {};
+  for(unsigned int i=0 ; i<NDIM; ++i) step[i] += 0.01;
+  double start[NDIM] = {};
   
   minimum->SetFunction(f);
 
-  for(unsigned int i=0 ; i <4; ++i){
+  for(unsigned int i=0 ; i<NDIM; ++i){
     minimum->SetVariable(i, Form("x%d",i), start[i], step[i]);
   }
 
-  // do the minimization
-  std::cout << "Numerical:" << std::endl;
   minimum->Minimize();
   const double *xs = minimum->X();
-  std::cout << "Minimum: f(" << xs[0] << "," << xs[1] <<  "," << xs[2] << "," << xs[3] << "): "
-	    << minimum->MinValue()  << std::endl;
+  std::cout <<  std::endl;
+  std::cout << "NUMERICAL:  f(";
+  for(unsigned int i=0 ; i<NDIM; ++i) std::cout << xs[i] << ",";
+  std::cout <<  "): " << minimum->MinValue()  << std::endl;
+  TVectorD x0(NDIM, start);
+  TMatrixD Binv(B);
+  TVectorD xs_ana = x0 - Binv.InvertFast()*(B*x0 + g);
+  std::cout << "ANALYTICAL: f(";
+  for(unsigned int i=0 ; i<NDIM; ++i) std::cout << xs_ana[i] << ",";
+  std::cout <<  "): " << Quadratic(xs_ana.GetMatrixArray()) << std::endl;
 
-  std::cout << "Analytical:" << std::endl;
-  TVectorD x0(4, start);
-  TVectorD xs_ana = x0 - B.InvertFast()*(B*x0 + g);
-  std::cout << "Minimum: f(" << xs_ana[0] << "," << xs_ana[1] <<  "," << xs_ana[2] << "," << xs_ana[3] << "): "
-            << Quadratic(xs_ana.GetMatrixArray())  << std::endl;
   return;
 }
